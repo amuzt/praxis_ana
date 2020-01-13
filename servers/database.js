84f0d423fda9dc71fa1a0c916d0f68be74539225
ana <amust93@gmail.com>
2020-01-13 16:11:03 +0700

    latihan hari keenam

---
+++ servers/database.js
@@ -0,0 +1,10 @@
+const mongoose  = require("mongoose")
+var host        = "mongodb://localhost:27017/day6"
+
+mongoose.connect(host, {
+    'useNewUrlParser': true,
+    'useUnifiedTopology': true
+
+})
+
+mongoose.set('useCreateIndex', true)
